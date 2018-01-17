5578e10d2adeee9b11955a0cb19f223922e038f8
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-17 12:21:56 -0800

    "stack functional shared now passes all tests"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+  stack.count = 0;
+  extend(stackMethods, stack);
+  return stack;
 };
+var extend = function (source, destination) {
+	for (var key in source) {
+		destination[key] = source[key];
+	}
+}
+var stackMethods = {
+	push : function (value) {
+		this.count ++;
+		this.storage[this.count] = value;
 
-var stackMethods = {};
+	},
+	pop : function () {
+		if (this.count !== 0) {
+			var result = this.storage[this.count];
+			delete this.storage[this.count];
+			this.count --;
+			return result;
+		}
+
+	},
+	size : function () {
+		return this.count;
+	}
+};
 
 
