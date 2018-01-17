0a97d50112a97e38b365500b0bda768db68c72eb
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-17 15:11:13 -0800

    "queue in es6 instantiation now works as intended"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.front = 1;
+  	this.storage = {};
+  }
+  enqueue(value) {
+  	this.count++;
+  	this.storage[this.count] = value;
+  }
+  dequeue() {
+  	if(this.size() !== 0) {
+  		var result = this.storage[this.front];
+  		delete this.storage[this.front];
+  		this.front ++;
+  		return result;
+  	}
+  }
+  size() {
+  	return (this.count - this.front + 1);
   }
 
 }
