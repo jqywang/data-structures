000b043845762b8ad139b1d1147f8763fb40b84e
ysera11 <hanyuzhu94@gmail.com>
2018-01-17 15:10:49 -0800

    es6 stack and queue - sprint completion

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    var temp = this.storage[this.count - 1];
+    delete this.storage[this.count - 1];
+    if(this.count !== 0){
+      this.count--;
+    }
+    return temp;
+  }
+
+  size() {
+    return this.count;
+  }
 }
