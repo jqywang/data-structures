8d8f58f199f70ded77e6b2f8da478104626d3b98
jqywang <jqywang.95@gmail.com>
2018-01-18 11:25:21 -0800

    "hash tables pass all tests besides collision"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,26 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[0];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var compareValue = this._storage.get(index);
+  var removeFunction = function (value, key, collection) {
+    if (value === compareValue) {
+      collection.splice(value, 1);
+      //break;
+    }
+  };
+  this._storage.each(removeFunction);
 };
 
 
