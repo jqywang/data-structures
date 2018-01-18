939cee7ab91952a160a7f8851426d35ac4a56f8f
jqywang <jqywang.95@gmail.com>
2018-01-18 10:00:44 -0800

    "data structure set now works as intended and passes all tests"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
+  set.toBeRemoved = -1;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      this.toBeRemoved = i;
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    this.storage.splice(this.toBeRemoved, 1);
+  }
 };
 
 /*
