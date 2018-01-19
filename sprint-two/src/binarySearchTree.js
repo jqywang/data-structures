f3c11af49ab3ca9c5f01822bbf1ff94f5214f975
jqywang <jqywang.95@gmail.com>
2018-01-18 18:22:14 -0800

    commit before branch

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,39 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(BSTMethods);
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
 
+var BSTMethods = {
+  //insert
+  insert: function(value) {
+    var newNode = BinarySearchTree(value);
+    if (this.value > value) {
+      if (this.left === null) {
+        this.left = newNode;
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value);
+      }
+    } 
+  },
+  contains: function(value) {
+    if (this.value === value) {
+      return true;
+    }
+    // recursion here 
+  },
+  depthFirstLog: function() {
+    
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
